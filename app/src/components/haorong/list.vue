deb1186663f9d8876268a8a19d5f52bdb8b6a79d
huangxin <huangxin@haodai.com>
2017-09-27 13:56:07 +0800

    删除scroll

---
+++ app/src/components/haorong/list.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+    }
+</script>
